23f914374fa246ce11dff61015b6a44b43ec0dfe
simgsr <simgsr@gmail.com>
2026-04-28 18:13:05 +0800

    chore: delete obsolete ingestion and bible modules

--- scratch/test_metadata.py
+++
@@ -1,7 +0,0 @@
-from src.ingestion.metadata_extractor import MetadataExtractor
-
-text = """Member’s Gui Theme Topic Speaker Date The spea physical function, other gro A similar together, through t the colou In this me important 4 Messa 1) W W ou 2) Fo CELL ide Effectiv We are Elder G 07th & 0 Spiri ker begins body in 1 the comm w in knowle analogy mi interlockin the hands o rful tapestr essage, the t it is that w ages on th We are the B We are joine urselves up our Characte a. Unity is wit the Sp b. Divers wante there c. Harm equal part is d. Matu L GU ve Priests an One Body oh Hock"""
-
-extractor = MetadataExtractor()
-result = extractor.extract(text)
-print(f"RESULT: {result}")
--- src/graph/state.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict, Annotated, List, Union
-from langchain_core.messages import BaseMessage
-
-class AgentState(TypedDict):
-    query: str
-    messages: List[BaseMessage]
-    response: str
-    chart_path: Union[str, None]
-    debug_log: str
